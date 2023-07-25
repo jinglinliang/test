7f02320abca827fed210f3785f94f9ab9ee1aad9
Jinglin Liang <jinglin@privatemachines.com>
2023-07-25 15:09:01 -0400

    Init project

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # test
+This application help you schedule movie
+
+## Set up
+You need to have `python3` installed
+```
+pip install dep.txt
+```
+## Usage
+```
+python3 aa.py
+```
---
+++ movie_scheduler.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env/ python3
+# Author: Foster
+
+"""Command line tool help you to schedule movie"""
+
+
+import argparse
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
